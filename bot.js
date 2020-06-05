d53c3336765c64ae4b54fe7503a87a22b4db3578
yuliayarmak <65883348+yuliayarmak@users.noreply.github.com>
2020-06-05 04:19:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const { Telegraf } = require('telegraf');
-const config = require('./config.json')
+const config = require('./config.json');
+const gameUrl = 'https://chadcancer.github.io/luckycard/';
+const gameUrl2 = 'https://chadcancer.github.io/hangman/';
 
 const bot = new Telegraf(config.token)
 bot.start((ctx) => ctx.reply('Здравствуйте! \n\nЯ могу предоставить Вам игры для бессмысленной траты времени и, в некоторых случаях, для повышения своей значимости в обществе. \n\nСписок доступных игор скоро обновится. Ожидайте!'))
