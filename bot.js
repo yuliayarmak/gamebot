c187e67cc296e4b9df4e13e845a5969ed580a677
yuliayarmak <65883348+yuliayarmak@users.noreply.github.com>
2020-06-05 23:46:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,13 +9,17 @@ const gameShortName = 'LuckyCards';
 const gameShortName2 = 'hangman';
 const gameShortName3 = 'TicTacToe';
 const gameShortName4 = 'twentyfourtyeight';
+let url;
 
 const bot = new Telegraf(config.token)
 bot.start((ctx) => ctx.reply('Здравствуйте! \n\nЯ могу предоставить Вам игры для бессмысленной траты времени и, в некоторых случаях, для повышения своей значимости в обществе. \n\nСписок доступных игор скоро обновится. Также, стоит уточнить, что игры будут доступны только для устройств системы IOS, для таких едениц как Iphone. В скорем времени мы это также исправим и игры будут доступны любому пользователю. Ожидайте!'))
 bot.help((ctx) => ctx.reply('Ничем не могу помочь. Это всё Ваша вина.'))
 
 bot.command('luckyCards',({ replyWithGame }) => replyWithGame(gameShortName));
-bot.gameQuery(({ answerGameQuery }) => answerGameQuery(gameUrl));
+bot.command('hangmen',({ replyWithGame }) => replyWithGame(gameShortName2));
+bot.command('twentyfourtyeight',({ replyWithGame }) => replyWithGame(gameShortName4));
+bot.command('TicTacToe',({ replyWithGame }) => replyWithGame(gameShortName3));
 
-bot.command('hangman',({ replyWithGame }) => replyWithGame(gameShortName2));
+bot.gameQuery(({ answerGameQuery }) => answerGameQuery(url));
+
 bot.launch()
