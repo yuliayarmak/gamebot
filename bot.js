6729725e24c06250c3342a1ffe5d761d74ed060b
yuliayarmak <65883348+yuliayarmak@users.noreply.github.com>
2020-06-05 05:29:57 +0300

    in the process of realization

--- bot.js
+++ bot.js
@@ -1,9 +1,17 @@
 const { Telegraf } = require('telegraf');
 const config = require('./config.json'); //здесь находиться токен
+
 const gameUrl = 'https://chadcancer.github.io/luckycard/';
 const gameUrl2 = 'https://chadcancer.github.io/hangman/';
+const gameShortName = 'LuckyCards';
+const gameShortName2 = 'hangman';
 
 const bot = new Telegraf(config.token)
 bot.start((ctx) => ctx.reply('Здравствуйте! \n\nЯ могу предоставить Вам игры для бессмысленной траты времени и, в некоторых случаях, для повышения своей значимости в обществе. \n\nСписок доступных игор скоро обновится. Ожидайте!'))
 bot.help((ctx) => ctx.reply('Ничем не могу помочь. Это всё Ваша вина.'))
+
+bot.command('luckyCards',({ replyWithGame }) => replyWithGame(gameShortName));
+bot.gameQuery(({ answerGameQuery }) => answerGameQuery(gameUrl));
+
+bot.command('hangmen',({ replyWithGame }) => replyWithGame(gameShortName2));
 bot.launch()
